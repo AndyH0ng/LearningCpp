00f3ff28358d7cf262ebf140d28d933d33adde78
AndyH0ng <chili.tomat0@icloud.com>
2023-12-25 13:54:54 +0900

    Initial Commit

---
+++ LearningCpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LearningCpp
+//
+//  Created by 홍준우 on 12/25/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
